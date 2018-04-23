2a66587493fa4eb64ff71318919154690ee7f6c6
seongjungkim <plznw4me@naver.com>
2018-04-23 23:09:24 +0900

    add batch norm_1

--- cnn.py
+++ cnn.py
@@ -51,13 +51,13 @@ def batch_norm_0(x,train_phase,scope_bn):
     updates_collections=None,
     is_training=True,
     reuse=None, # is this right?
-    trainable=True,
+    trainable=train_phase,
     scope=scope_bn)
     bn_inference = batch_norm(x, decay=0.999, center=True, scale=True,
     updates_collections=None,
     is_training=False,
     reuse=True, # is this right?
-    trainable=True,
+    trainable=train_phase,
     scope=scope_bn)
     z = tf.cond(train_phase, lambda: bn_train, lambda: bn_inference)
     return z
